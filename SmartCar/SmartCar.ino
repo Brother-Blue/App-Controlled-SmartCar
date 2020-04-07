5b61c4c0fd27ec3e58a4e1a1edfe1ac45b251f5b
Clementine Jensen <gusjensecl@student.gu.se>
2020-04-07 11:41:48 +0200

    Add Car Control to the SmarCar
    Add the necessary implementation for the SmartCar
    to be controlled through input.
    Add the handleInput method, constants for speed,
    and turning degrees.
    
    As a result, the car should be able to
    be controlled through input.

--- SmartCar/SmartCar.ino
+++ SmartCar/SmartCar.ino
@@ -4,6 +4,11 @@ BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
 
+const int fSpeed   = 70;  // 70% of the full speed forwards
+const int bSpeed   = -70; // 70% of the full speed backwards
+const int lDegrees = -75; // Degrees to turn left
+const int rDegrees = 75;  // Degrees to turn right
+
 SimpleCar car(control);
 
 const int minObstacle = 20;
@@ -21,13 +26,48 @@ void setup(){
 }
 
 void loop() {
+  
+  handleInput();
+  
  distance = front.getDistance();
  Serial.println(distance);
   
   
  if(distance < minObstacle && distance > 0){
   car.setSpeed(0);
+  }
  }
- 
+
+ void handleInput(){ // Handle serial input if there is any
+  
+    if (Serial.available()){
+        char input = Serial.read();
+                                    
+        switch (input){
+        case 'l': // Turn Left
+            car.setSpeed(fSpeed);
+            car.setAngle(lDegrees);
+            break;
+
+        case 'r': // Turn Right
+            car.setSpeed(fSpeed);
+            car.setAngle(rDegrees);
+            break;
+
+        case 'f': // Foward
+            car.setSpeed(fSpeed);
+            car.setAngle(0);
+            break;
+
+        case 'b': // Reverse
+            car.setSpeed(bSpeed);
+            car.setAngle(0);
+            break;
+
+        default: 
+            car.setSpeed(0);
+            car.setAngle(0);
+        }
+    }
 }
  
