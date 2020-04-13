879ee5c333659a8178b25875d11d358f83b714fe
hjathu <hjathu@student.chalmers.se>
2020-04-13 14:13:22 +0200

    Changed to correct odometer pin value & added printers in loop

--- SmartCar/SmartCar.ino
+++ SmartCar/SmartCar.ino
@@ -7,20 +7,18 @@ DifferentialControl control(leftMotor, rightMotor);
 
 BluetoothSerial bluetooth;
 
-GY50 gyroscope(37);
+const int GYROSCOPE_OFFSET = 37;
+GY50 gyro(GYROSCOPE_OFFSET); // The offset we have acquired via the getOffset method
 
-ArduinoRuntime runtime;
-
-unsigned short ODOMETER_PIN = 32;
-unsigned long PULSES_PER_METER = 110;
-DirectionlessOdometer odometer(ODOMETER_PIN,[]() { odometer.update(); }, PULSES_PER_METER);
+const unsigned long PULSES_PER_METER = 600;
+DirectionlessOdometer odometer(smartcarlib::pins::v2::leftOdometerPin,[]() { odometer.update(); }, PULSES_PER_METER);
 
 const int fSpeed   = 70;  // 70% of the full speed forwards
 const int bSpeed   = -70; // 70% of the full speed backwards
 const int lDegrees = -75; // Degrees to turn left
 const int rDegrees = 75;  // Degrees to turn right
 
-SmartCar car(control, gyroscope, odometer);
+SmartCar car(control, gyro, odometer);  // Initializing of the car
 
 const int minObstacle = 20;
 const int targetSpeed = 35;
@@ -64,14 +62,14 @@ void handleInput(){ // Handle serial input if there is any
             car.setAngle(0);
             break;
         
-        case 'c': //Increases carspeed by 10%
+        case 'c': // Increases carspeed by 10%
             //car.setSpeed(car.getSpeed() + 10);
             break;
 
-        case 'd': //Decreases carspeed by 10%
-            //car.setSpeed(car.getSpeed() - 10);
+        case 'd': // Decreases carspeed by 10%
+            // car.setSpeed(car.getSpeed() - 10);
             break;
-            //TODO Implement SmartCar to access getSpeed-method. See new issue in Git
+            // TODO Implement SmartCar to access getSpeed-method. See new issue in Git
         default: 
             car.setSpeed(0);
             car.setAngle(0);
@@ -81,16 +79,19 @@ void handleInput(){ // Handle serial input if there is any
 
 void inputHandler() {
   if (bluetooth.available()) {
-    //Handle user inputs
+    // Handle user inputs
   }
 }
 void loop() {
 
  handleInput();
   
- distance = front.getDistance();
- Serial.println(distance);
+ Serial.println(front.getDistance()); // Ultrasonic sensor
   
+ Serial.println(odometer.getDistance()); // Odometer
+
+ gyro.update();
+ Serial.println(gyro.getHeading()); // Gyroscope
   
  if(distance < minObstacle && distance > 0){
   car.setSpeed(0);
