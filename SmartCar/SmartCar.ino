e3f11018a6867cd64801faf0407a2456fc103026
Christian O'Neill <gusonech@student.gu.se>
2020-04-23 17:31:09 +0200

    Updated the self drive methods. Added in checkDistance into loop for manual mode. Initalized VL53L0X component according to documentation.
    Co-authored-by: Hugo Hempel <hugohhempel@gmail.com>

--- SmartCar/SmartCar.ino
+++ SmartCar/SmartCar.ino
@@ -10,24 +10,20 @@ const int turnAngle = 75;   // Degrees to turn
 const int minObstacle = 20; // Minimum distance ahead to obstacle
 const int GYROSCOPE_OFFSET = 37;
 unsigned int error = 0; //FIXME: Need to calculate error for LIDAR sensor readings.
+unsigned int frontDistance;
+unsigned int backDistance;
 const unsigned int MAX_DISTANCE = 100; // Max distance to measure with ultrasonic
-
-float frontDistance;
-float backDistance;
 
 boolean atObstacle = false;
 
 //FIXME: Make sure ultrasonic pins are correct for side or behind :)
 const int TRIGGER_PIN = 5; // Trigger signal for ultrasonic
-const int ECHO_PIN = 18; // Reads signal for ultrasonic
-
-//FIXME: Make sure this is the correct pin on the vehicle :)
-const int SIDE_FRONT_PIN = A0; // LIDAR Sensor pin
+const int ECHO_PIN = 18;   // Reads signal for ultrasonic
 
 const unsigned long PULSES_PER_METER = 600; // Amount of odometer pulses per 1 meter
 
 SR04 back(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // Ultrasonic sensor
-VL53L0X frontSensor;
+VL53L0X frontSensor; // Micro LIDAR sensor
 
 GY50 gyro(GYROSCOPE_OFFSET); // Gyroscope sensor const int rDegrees = 75;  // Degrees to turn right
 
@@ -45,12 +41,17 @@ SmartCar car(control, gyro, leftOdometer, rightOdometer); // Initializing of the
 void setup()
 {
     Serial.begin(9600);
-    frontSensor.init();
     Wire.begin();
+    frontSensor.setTimeout(500);
+    if (!frontSensor.init()) 
+    {
+        Serial.print("Failed to initialize VL53L0X sensor.");
+        while (1) {}
+    }    
+
+    frontSensor.startContinuous(100);
     bluetooth.begin("Group 2 SmartCar");
     Serial.print("Ready to connect!");
-    frontSensor.setTimeout(500);
-    frontSensor.startContinuous(100);
 }
 
 void turnRight(int degrees, int turnSpeed = speed)
@@ -67,18 +68,18 @@ void turnLeft(int degrees, int turnSpeed = speed)
 
 void driveForward(int driveSpeed = speed)
 {
-    if (car.getSpeed() < 0)
-    {                                // Fixes motor overloading
-        while (car.getSpeed() < -20) // Used -20 as a threshold
+    if (car.getSpeed() > 0)
+    {                     
+        while (car.getSpeed() > 20) // Used 20 as a threshold
         {
-            car.setSpeed(car.getSpeed() + 10);
+            car.setSpeed(car.getSpeed() - 10);
         }
     }
     car.setSpeed(driveSpeed);
     car.setAngle(0);
 }
 
-void driveBackward(int driveSpeed = speed)
+void driveBackward(int driveSpeed = -speed)
 {
     if (car.getSpeed() > 0)
     {                               // Fixes motor overloading
@@ -91,9 +92,27 @@ void driveBackward(int driveSpeed = speed)
     car.setSpeed(driveSpeed);
 }
 
-void brake() {
+void brake()
+{
     car.setSpeed(0);
     car.setAngle(0);
+}
+
+void checkDistance()
+{
+    frontDistance = (frontSensor.readRangeSingleMillimeters() / 10) - error; //Divided by 10 to convert to cm
+    backDistance = back.getDistance();
+    if (frontSensor.timeoutOccurred()) { Serial.print("VL53L0X sensor timeout occurred."); }
+
+    if (frontDistance > 0 && frontDistance <= minObstacle)
+    {
+        atObstacle = true;
+        brake();
+    }
+    if (backDistance > 0 && backDistance <= minObstacle)
+    {
+        brake();
+    }
 }
 
 boolean tryTurning()
@@ -106,7 +125,7 @@ boolean tryTurning()
             // Increment steering to the right and check if space is free
             turnRight(i, 20);
             delay(100);
-            if (frontDistance > 65) // 65 was used as 78 is the max range for the LIDAR sensor
+            if (frontDistance > 85) // 85 was used as 100 is the max range for the LIDAR sensor
             {
                 atObstacle = false;
                 break;
@@ -118,7 +137,7 @@ boolean tryTurning()
             // Increment steering to the right and check if space is free
             turnLeft(i, 20);
             delay(100);
-            if (frontDistance > 65) // 65 was used as 78 is the max range for the LIDAR sensor
+            if (frontDistance > 85) // 85 was used as 100 is the max range for the LIDAR sensor
             {
                 atObstacle = false;
                 break;
@@ -133,27 +152,21 @@ boolean tryTurning()
 
 void driveWithAvoidance()
 {
-    if (!atObstacle) { driveForward(speed); }
-    frontDistance = (frontSensor.readRangeContinuousMillimeters()*10) - error; //Multiplied by 10 to convert to cm
-    if (backDistance > 0 && backDistance <= minObstacle)
+    if (!atObstacle)
     {
-        brake();
+        checkDistance();
+        driveForward(speed);
     }
-    if (frontDistance > 0 && frontDistance <= minObstacle)
+    while (atObstacle && backDistance >= minObstacle)
     {
-        atObstacle = true;
-        brake();
-        while (atObstacle && backDistance >= minObstacle)
+        driveBackward(-10);
+        if (backDistance <= minObstacle)
         {
-            driveBackward(-10);
-            backDistance = back.getDistance();
-            if (backDistance <= minObstacle) {
-                brake();
-            }
-            delay(500);   //FIXME: Linus please check how far the car goes during a 500 delay. :)
             brake();
-            atObstacle = tryTurning();
         }
+        delay(500); //FIXME: Linus please check how far the car goes during a 500 delay. :)
+        brake();
+        atObstacle = tryTurning();
     }
 }
 
@@ -207,5 +220,6 @@ void readBluetooth()
 
 void loop()
 {
-    readBluetooth(); //manual
+    readBluetooth(); 
+    checkDistance(); // Checks distance in manual mode.
 }
