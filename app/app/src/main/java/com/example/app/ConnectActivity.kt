d5787542784d6b91e7fe53c26312017acee81713
Linus Ivarsson <linusivarsson@live.se>
2020-04-28 12:56:16 +0200

    Reworked Bluetooth-connection

--- app/app/src/main/java/com/example/app/ConnectActivity.kt
+++ app/app/src/main/java/com/example/app/ConnectActivity.kt
@@ -1,61 +1,59 @@
 package com.example.app
 
+//import android.app.ProgressDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
 import android.content.Context
-import android.content.Intent
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.ProgressBar
-import android.widget.Spinner
-import androidx.core.view.isVisible
-import kotlinx.android.synthetic.main.activity_connect.*
 import java.io.IOException
 import java.util.*
-import me.aflak.bluetooth.Bluetooth
 
 class ConnectActivity : AppCompatActivity() {
 
-
-    companion object {
+    companion object{
         var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
         var m_bluetoothSocket: BluetoothSocket? = null
-        lateinit var m_bluetoothAdapter: BluetoothAdapter
+        //var m_progress: ProgressDialog? = null
+        var m_bluetoothAdapter: BluetoothAdapter? = null
         var m_isConnected: Boolean = false
-        lateinit var m_address: String
-
+        var m_address: String? = null
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect)
-        m_address = "FC:F5:C4:0F:87:62"
+        //Get the car MAC-address that is set in MainActivity
+        m_address = intent.getStringExtra(MainActivity.CAR_ADDRESS)
         ConnectToDevice(this).execute()
-        var spinner: ProgressBar = findViewById(R.id.progressBar)
-
-        //control_led_on.setOnClickListener { sendCommand("a") }
-        //control_led_off.setOnClickListener { sendCommand("b") }
-        //control_led_disconnect.setOnClickListener { disconnect() }
     }
 
+    //Class in charge of connecting the device with the car
+    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>(){
 
-    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
         private var connectSuccess: Boolean = true
         private val context: Context
 
+        //Constructor
         init {
             this.context = c
         }
 
+        override fun onPreExecute() {
+            super.onPreExecute()
+            //m_progress = ProgressDialog.show(context, "Connecting...", "please wait")
+        }
+        //Connect device to car
         override fun doInBackground(vararg params: Void?): String? {
-            try{
-                if (m_bluetoothSocket == null || !m_isConnected) {
+            try {
+                if (m_bluetoothSocket == null || !m_isConnected){
                     m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-                    val device: BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(m_address)
+                    val device: BluetoothDevice = m_bluetoothAdapter!!.getRemoteDevice(m_address)
                     m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
+                    //Stop looking for other devices to save battery
                     BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
                     m_bluetoothSocket!!.connect()
                 }
@@ -68,12 +66,13 @@ class ConnectActivity : AppCompatActivity() {
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            if(!connectSuccess) {
-                Log.i("data", "couldn't connect")
+            if(!connectSuccess){
+                Log.i("data", "could not connect")
             } else {
                 m_isConnected = true
-
             }
+            //m_progress?.dismiss()
+
         }
     }
 }
