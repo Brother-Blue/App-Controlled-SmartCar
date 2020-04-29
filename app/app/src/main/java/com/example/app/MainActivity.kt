ab5141d8b8af583e2542412197bafc3e4f3edadb
hjalmar <hjalmar.thunberg@gmail.com>
2020-04-29 13:25:40 +0200

    Changed some app bluetooth functions
    Co-authored-by: Christian O'Neill <gusonech@student.gu.se>

--- app/app/src/main/java/com/example/app/MainActivity.kt
+++ app/app/src/main/java/com/example/app/MainActivity.kt
@@ -18,24 +18,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Set m_bluetoothAdapter to devices Bluetooth
+        // Set m_bluetoothAdapter to devices Bluetooth
         m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
-        //Check to see if device support Bluetooth
+        // Check to see if device supports Bluetooth
         if(m_bluetoothAdapter == null){
             toast("Device does not support Bluetooth")
         }
-        //Check to see if devices bluetooth is enabled. If not, prompt the user to enable it.
-        if(!m_bluetoothAdapter!!.isEnabled){
+        
+        // Check to see if devices bluetooth is enabled. If not, prompt the user to enable it.
+        if(m_bluetoothAdapter.isEnabled == false){
             val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
         }
 
-        //When clicking on the connect button, call connectCar-function
+        // When clicking on the connect button, call connectCar-function
+        // Disables the connect button until bluetooth is enabled
+        while (!Bluetooth.isEnabled()) {
+            connect_button.setEnabled(false);
+        }
         connect_to_car.setOnClickListener{ connectCar()}
-
     }
-    //Change view to the one when the car is connected
+    // Change view to the one when the car is connected
     private fun connectCar() {
         val intent = Intent(this, ConnectActivity::class.java)
         startActivity(intent)
