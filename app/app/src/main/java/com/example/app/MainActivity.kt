4efe29f7e1e2e9ff989a6ad9be8d208f414e6bf0
hjalmar <hjalmar.thunberg@gmail.com>
2020-04-29 14:53:10 +0200

    Updated app bluetooth and added button function
    Co-authored-by: Christian O'Neill <gusonech@student.gu.se>

--- app/app/src/main/java/com/example/app/MainActivity.kt
+++ app/app/src/main/java/com/example/app/MainActivity.kt
@@ -9,37 +9,52 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
 
+
 class MainActivity : AppCompatActivity() {
 
-    private var m_bluetoothAdapter: BluetoothAdapter? = null
     private val REQUEST_ENABLE_BLUETOOTH = 1
+    private val mBluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toggleConnectButton(false)
 
-        // Set m_bluetoothAdapter to devices Bluetooth
-        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-
-        // Check to see if device supports Bluetooth
-        if(m_bluetoothAdapter == null){
-            toast("Device does not support Bluetooth")
+        // Check to see if device support Bluetooth
+        if (mBluetoothAdapter == null) {
+            toast("This device does not support Bluetooth.")
         }
-        
         // Check to see if devices bluetooth is enabled. If not, prompt the user to enable it.
-        if(m_bluetoothAdapter.isEnabled == false){
+        if (mBluetoothAdapter?.isEnabled == false) {
             val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+        } else {
+            toggleConnectButton(true)
         }
-
         // When clicking on the connect button, call connectCar-function
-        // Disables the connect button until bluetooth is enabled
-        while (!Bluetooth.isEnabled()) {
-            connect_button.setEnabled(false);
-        }
         connect_to_car.setOnClickListener{ connectCar()}
     }
-    // Change view to the one when the car is connected
+
+    // Reads return value from startActivityForResult().
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) { // Using if statement to specify which request code to listen for.
+            when (resultCode) {
+                RESULT_OK -> {
+                    toggleConnectButton(true)
+                }
+                else -> {
+                    toast("Bluetooth is required for this application.")
+                }
+            }
+        }
+    }
+
+    private fun toggleConnectButton(value: Boolean) {
+        connect_to_car.isEnabled = value
+    }
+
+    //Change view to the one when the car is connected
     private fun connectCar() {
         val intent = Intent(this, ConnectActivity::class.java)
         startActivity(intent)
